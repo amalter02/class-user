84ba0b0b1e319ad2cffd8bd589a15439725623f0
amalter02 <56536314+amalter02@users.noreply.github.com>
2020-04-09 12:00:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     </head>
     <body>
         <?php echo "<h1>Bienvenu dans l'arrene</h1>";
-
             $Utilsateur1 = new user();
 
         ?>
@@ -20,28 +19,11 @@
                     $isConnect = $Utilsateur1->verifMpd($_POST['nom'],$_POST['mdp']);
                     if($isConnect){echo "connecté";
                     
-                        try{
-                            //execution du code sur la BDD exemple
-                           $maBase=new PDO('mysql:host=127.0.0.1; dbname=user; charset=utf8','root', 'root');
-                           echo "select * from 'users'" ;
-                           $ResultRequet = $maBase->query("select * from 'users'"); 
-                           echo "<table>";
-                           while ($TableauDeDonnee = $ResultRequet->fetch())
-                            {
-                                echo "<tr>";
-                                echo '<td>'.$TableauDeDonnee["nom"].' </td>';
-                                echo "<tr>";
-                            }
-                            $ResultRequet->closeCursor();
-                            echo "</table>";
-
-                        }
-                        catch (Exception $erreur){
-                            echo $erreur->getMessage();
-                         }
+                        
                     
-                    }else{echo "erreur";}
+                  
                 }
+            }
                 
            ?>
     </body>
